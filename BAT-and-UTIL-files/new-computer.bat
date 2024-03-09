41cfe620d0bf8f66fa47c809cb361213e9629ae0
ClioCJS <ClioCJS@gmail.com>
2024-03-08 22:30:11 -0500

    Fri 10:30PM: Fea - Let Me Down

---
+++ BAT-and-UTIL-files/new-computer.bat
@@ -0,0 +1,47 @@
+@Echo OFF
+
+
+
+
+::::: BOOKMARKS:
+    set CALIBRATION_URL=http://www.lagom.nl/lcd-test/
+
+
+
+
+
+::::: PRE-CHECK:
+    call checkeditor
+    call validate-environment-variables BAT MACHINENAME
+
+::::: SETUP:
+    %BAT%\
+
+::::: GET COMPUTER NAME:
+    color yellow on black %+ echo What will this computer be named? %+ color white on black 
+     set NEW_COMPUTER_NAME=?????
+    eset NEW_COMPUTER_NAME
+
+
+::::: EDIT BAT/ENVIRONMENT FILES:
+    %BAT%\
+    %EDITOR% environm.btm allcomputers.bat allcomputersexceptself.bat backup-stuff.BAT drives.bat display-drive-mapping.bat fr.bat wake-computers.bat
+
+::::: HOSTS?
+    echo. %+ call warning "Hey, we probably need to edit hosts to put this computer in." %+ pause %+ color white on black
+    call hosts
+
+::::: VALIDATE PERL SCRIPTS:
+    %BAT%
+    call validate-all-perl-files
+    echo.
+    call success "Perl files validated"
+    echo. %+ echo. %+ echo. %+ echo. %+ echo. %+ echo. %+ echo. %+ echo. 
+
+::::: OTHER REMINDERS:    
+    pause %+ call advice "if we still command-line-blog: Test mtsend.bat, install python-2.5.2.msi for mtsend" 
+    pause %+ call warning "Change EditPlus preferences to syntax-highlight TXT files as .CPP files"            
+    pause %+ call warning "After installing perl to c:\perl\, run c:\bat\setup-perl-junctions-on-new-computer.bat"            
+    pause %+ call warning "Run install-common-programs-with-winget.bat"            
+    pause %+ call warning "Let's calibrate our monitor at %ITALICS_ON%%CALIBRATION_URL%%ITALICS_OFF%"
+    pause %+ %CALIBRATION_URL%
---
+++ BAT-and-UTIL-files/wake-computers.bat
@@ -0,0 +1,23 @@
+@echo OFF
+
+: EXAMPLE:
+:    C:\new>mcgetmac goliath
+:    Name: Goliath
+:    IP address: 66.167.233.212
+:    Ethernet MAC address: BC:5F:F4:43:EC:6E
+
+rem  Assuming we're running this on Demona, so don't need Demona in here:
+        call validate-environment-variables  IP_WYVERN  IP_THAILOG  IP_GOLIATH
+        call validate-environment-variables MAC_WYVERN MAC_THAILOG MAC_GOLIATH
+        gosub wake_computer                     Wyvern
+        gosub wake_computer                                Thailog
+        gosub wake_computer                                            Goliath
+
+
+goto :END
+    :wake_computer [name]
+        set tmp_emoji=%[emoji_machine_%name%]
+        call less_important "Waking %tmp_emoji% %name% %tmp_emoji%..."
+        echo wake-on-lan.exe %[mac_%name%] /a %[ip_%name%]
+    return
+:END
