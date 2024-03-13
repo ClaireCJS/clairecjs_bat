25b3d0015588b26085a09f79caaa44b945b7f409
ClioCJS <ClioCJS@gmail.com>
2024-03-13 11:25:41 -0400

    Wed 11:24AM: Fea - Mujer Moderna - new CLI speech using wsay.exe

--- BAT-and-UTIL-files/speak.bat
+++ BAT-and-UTIL-files/speak.bat
@@ -12,7 +12,34 @@ if "%SLEEPING%" eq "1" goto :Asleep              %+ REM don't speak loud words i
 
 
     :Awake
-        speak.exe %*                            %+ REM Just speak normally if no one is sleeping
+        rem Method 1: speak.exe %*                            %+ REM Just speak normally if no one is sleeping
+        rem Method 2: more reliable: mshta vbscript:Execute("CreateObject(""SAPI.SpVoice"").Speak(""%*"")(window.close)")
+
+        rem Method 3: 2024 new replacement which works through ALL sound devices simultaneously which is pretty amazing:
+        call validate-in-path wsay
+        wsay "%@UNQUOTE[%*]" --volume 100 --voice 3 --playback_device all
+
+
+        rem The voices I had when I set this to 3 (Zira):
+            rem C:\>wsay -l
+            rem  1 : Microsoft David Desktop - English (United States)
+            rem  2 : Microsoft Hazel Desktop - English (Great Britain)
+            rem  3 : Microsoft Zira Desktop - English (United States)
+            rem  4 : Microsoft David - English (United States)
+            rem  5 : Microsoft James - English (Australia)
+            rem  6 : Microsoft Linda - English (Canada)
+            rem  7 : Microsoft Richard - English (Canada)
+            rem  8 : Microsoft George - English (United Kingdom)
+            rem  9 : Microsoft Hazel - English (United Kingdom)
+            rem 10 : Microsoft Susan - English (United Kingdom)
+            rem 11 : Microsoft Sean - English (Ireland)
+            rem 12 : Microsoft Heera - English (India)
+            rem 13 : Microsoft Ravi - English (India)
+            rem 14 : Microsoft Catherine - English (Australia)
+            rem 15 : Microsoft Mark - English (United States)
+            rem 16 : Microsoft Zira - English (United States)
+
+
     goto :END
 
 :END
