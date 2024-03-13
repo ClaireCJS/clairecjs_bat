cd892235953fcfddb3ba92b7832e29fb67c9ba3f
ClioCJS <ClioCJS@gmail.com>
2024-03-13 17:11:57 -0400

    Wed 5:10PM: Fea - Out the Window

--- BAT-and-UTIL-files/git-commit.bat
+++ BAT-and-UTIL-files/git-commit.bat
@@ -55,7 +55,8 @@ REM Actually commit the files
 
 REM Remind that commit and push are not the same
     echo.
-    call warning "* The files were not uploaded, though. To do that: 'push' (which which executes 'git push origin main')"
+    call warning "The files were not uploaded, though"
+    call advice "To do that: '%italics%push%italics_off%' (which which executes '%italics%git push origin main%italics_off%')"
     if %NO_PAUSE ne 1 keystack push
 
 
