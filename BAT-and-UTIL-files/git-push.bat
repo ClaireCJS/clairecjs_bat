a4f26709b025dbd46af81b4efbff9494000f6565
ClioCJS <ClioCJS@gmail.com>
2024-03-14 09:17:40 -0400

    Thu 9:17AM: Fea - La Llorona - removed tee from git push

--- BAT-and-UTIL-files/git-push.bat
+++ BAT-and-UTIL-files/git-push.bat
@@ -20,7 +20,10 @@ set GIT_OUT=git.out
 echo.
 REM call unimportant "[Unfiltered GIT output followed by filtered GIT output]..."
 set TEECOLOR=%COLOR_UNIMPORTANT%
-call git push origin main %PARAMS% |& tee %GIT_OUT%
+REM old, but now git has it's own TEE to git.out internally: 
+REM      call git push origin main %PARAMS% |& tee %GIT_OUT%
+REM new:
+call git push origin main %PARAMS% 
 call errorlevel "Advice: for 'updates were rejected because the remote contains work that you do not have locally', you may need to 'git pull origin main' to merge and then try again" 
 
 echo.
