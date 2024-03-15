3364d63ee2dec3e46acdb74fd2d243d893b9c699
ClioCJS <ClioCJS@gmail.com>
2024-03-15 13:37:32 -0400

    Fri 1:37PM: Fea - Mujer Moderna

---
+++ BAT-and-UTIL-files/mydrives.bat
@@ -0,0 +1,15 @@
+@Echo OFF
+ echo.
+
+call validate-environment-variables MACHINENAME DRIVES_%MACHINENAME% MAX_MACHINENAME_LENGTH
+
+set STUFF=
+%COLOR_IMPORTANT% %+                                                    gosub TellForMachine %MACHINENAME%
+%COLOR_SUBTLE% %+ for %ma in (%ALL_COMPUTERS%) if %ma ne %MACHINENAME% (gosub TellForMachine %ma%)
+echo %STUFF% |:u8 call highlight-by-computer-name
+
+goto :END
+    :TellForMachine [MACHINENAME]
+        SET STUFF=%STUFF%%ANSI_RESET%%EMOJI_COMPUTER_DISK% %BOLD_OFF%Drives for %@FORMAT[-%MAX_MACHINENAME_LENGTH%,%MACHINENAME%] are: %ITALICS_ON%%BOLD_ON%%[DRIVES_%MACHINENAME%]%BOLD_OFF%%ITALICS_OFF% %NEWLINE% 
+    return
+:END
