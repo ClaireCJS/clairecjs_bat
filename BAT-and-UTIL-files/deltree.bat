18ad77d04b6ff825e8401f2f48673391586fbd46
ClioCJS <ClioCJS@gmail.com>
2024-03-15 13:18:11 -0400

    Fri 1:18PM: Fea - La Llorona

--- BAT-and-UTIL-files/deltree.bat
+++ BAT-and-UTIL-files/deltree.bat
@@ -7,13 +7,13 @@ if not isdir %DIR (
 )
 
 if isdir %DIR% (
-    call askyn "%EMOJI_NO_ENTRY% Delete folder '%italics%%DIR%%italics_off%' %blink%%underline%completely%underline_off%%blink_off%" no
+    call askyn "%EMOJI_NO_ENTRY% Delete folder '%italics%%@UNQUOTE[%DIR]%%italics_off%' %blink%%underline%completely%underline_off%%blink_off%" no
     if %ANSWER eq Y (
         %COLOR_REMOVAL% 
         echo. 
-        echo %EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%ITALICS_ON%%FAINT_ON%``
-        *del /a: /f /k /s /t /x /z /Nj /P /E %DIR%
-        echo %ITALICS_OFF%%FAINT_OFF%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%``
+        echo %EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%ITALICS_ON%%FAINT_ON%``
+        echo r | *del /a: /f /k /s /t /x /z /Nj /P /E %DIR%
+        echo %ITALICS_OFF%%FAINT_OFF%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%%EMOJI_NO_ENTRY%``
     )
 )
 
