d53968ee53c3019b4234608a09ad502834051d7e
ClioCJS <ClioCJS@gmail.com>
2024-07-26 06:30:01 -0400

    Fri 6:24AM: Home Movies: Shannon - Shannon's Song

--- BAT-and-UTIL-files/insert-foldername-into-end-of-filenames.bat
+++ BAT-and-UTIL-files/insert-foldername-into-end-of-filenames.bat
@@ -14,10 +14,12 @@ goto :Cleanup
 
             :processfile [FILE_QUOTED]
                 set FILE_UNQUOTED=%@unquote[%file_quoted]
-                echo %ANSI_COLOR_DEBUG%* Processing file %file_quoted%...
+                *cls
+                echo.
+                echo %ANSI_COLOR_DEBUG%%STAR% Processing file %emphasis%%file_quoted%%deemphasis%...
 
                 if "%file_unquoted" eq "%inserttrigger%" .or. %@REGEX[%parent_folder_name,%file_unquoted] eq 1 .or. %@REGEX[%inserttrigger,%file_unquoted] eq 1 (
-                        echo %ANSI_COLOR_WARNING%Skipping file '%file_unquoted%'...
+                        echos %ANSI_COLOR_WARNING%Skipping file '%file_unquoted%'...``
                         goto :Skip
                 )
 
@@ -26,9 +28,11 @@ goto :Cleanup
                     set target=%@name[%file_unquoted]-%parent_folder_name%-%_PID-%_DATETIME.%@ext[%file_unquoted]
                 )
 
-                echos %@RandFG[]
-                ren "%file_unquoted%" "%target%"
-
+                echos %@RandFG[]                    ``
+                ren "%file_unquoted%" "%target%" 
+                rem /Ns option on `ren` doesn't work right so no way to suppress '1 file renamed' easily
+                rem piping to this slows it down: | findstr  /v "file.renamed"
+                echo.
                 :Skip
             return
 
