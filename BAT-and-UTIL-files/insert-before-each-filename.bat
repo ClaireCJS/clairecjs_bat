be621a6077d088ddf8960c5ef58f4dc1ec31c2bc
ClioCJS <ClioCJS@gmail.com>
2024-08-09 08:55:27 -0400

    Fri 8:55AM: Jack Mackerel And The Smoltz - Ooh Va La

---
+++ BAT-and-UTIL-files/insert-before-each-filename.bat
@@ -0,0 +1,44 @@
+@Echo Off
+
+set INSERT_TEXT=%1
+if "%1" eq "SetVarsOnly" (goto :END)
+
+call validate-environment-variable INSERT_TEXT
+
+rem if exist "%inserttrigger%" (
+rem     call warning "Already did this folder. Aborting. (inserttrigger file of %inserttrigger% exists)"
+rem     goto :END
+rem )
+
+for /h %file in (*) do (gosub processfile "%@UNQUOTE[%file%]")
+goto :Cleanup
+
+            :processfile [FILE_QUOTED]
+                set FILE_UNQUOTED=%@unquote[%file_quoted]
+                *cls
+                echo.
+                echo %ANSI_COLOR_DEBUG%%STAR% Processing file %emphasis%%file_quoted%%deemphasis%...
+
+                if "%@INSTR[0,%@LEN[%insert_text],%file_unquoted]" eq "%@UNQUOTE[%insert_text%]" (
+                        echos      %ANSI_COLOR_WARNING%Skipping file '%file_unquoted%'...``%ANSI_COLOR_NORMAL%
+                        goto :Skip
+                )
+
+                set target=%INSERT_TEXT%%file_unquoted%
+                if exist "%target%" (
+                    set target=%INSERT_TEXT%%@name[%file_unquoted]-%_PID-%_DATETIME.%@ext[%file_unquoted]
+                )
+
+                echos %@RandFG[]                    ``
+                ren "%file_unquoted%" "%target%" 
+                rem /Ns option on `ren` doesn't work right so no way to suppress '1 file renamed' easily
+                rem piping to this slows it down: | findstr  /v "file.renamed"
+                echo.
+                :Skip
+            return
+
+
+:Cleanup
+rem >"%inserttrigger%"
+
+:END
