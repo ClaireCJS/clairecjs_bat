9619bfa2c6c29eb0f573c84b58661298b55781df
ClioCJS <ClioCJS@gmail.com>
2024-08-19 13:18:02 -0400

    Mon 1:17PM: Dan Vapid And The Cheats - Dead Roses

--- BAT-and-UTIL-files/convert-each-line-to-a-randomly-colored-dot.pl
+++ BAT-and-UTIL-files/convert-each-line-to-a-randomly-colored-dot.pl
@@ -1,15 +1,13 @@
-# Turn on autoflush for STDOUT
-$| = 1;
+$| = 1;														# Turn on autoflush for STDOUT for best performance
+print "\e[0m";												# reset ansi colors
+my $last_randcolor;											# track last color used to prevent duplicates
+my $curr_randcolor;                                         # current randomly-generated color
 
-print "\e[0m";
-
-while (<>) { 
-	# Generate a random color code (30-37)
-	my $color_code = int(rand(8)) + 30;
-
-	# ANSI escape sequence for setting foreground color
-	my $ansi_rand_fg = "\e[${color_code}m";
-
-	# Print text with random foreground color
-	print "${ansi_rand_fg}.";
+while (<>) {												# while STDIN exists
+	$curr_randcolor = int(rand(8)) + 30;					# Generate a random color code (30-37)
+	while ($curr_randcolor == $last_randcolor) {			# if the color is the same as last
+		$curr_randcolor = int(rand(8)) + 30;				# then re-generate it until it isn't
+	}
+	$last_randcolor = $curr_randcolor;						# keep track of color so next line isn't the same color
+	print "\e[${curr_randcolor}m";							# ANSI escape sequence for setting foreground color
 }
