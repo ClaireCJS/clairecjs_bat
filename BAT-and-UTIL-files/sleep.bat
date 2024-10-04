7d41da8a466927696b55d68d9ae4ba2d84472961
ClioCJS <ClioCJS@gmail.com>
2024-10-04 17:00:23 -0400

    Fri 5:00PM: -

--- BAT-and-UTIL-files/sleep.bat
+++ BAT-and-UTIL-files/sleep.bat
@@ -1,16 +1,23 @@
 @Echo OFF
-goto :%OS
+
+rem Before Windows  7, we used a 32-bit sleep.exe in our %UTIL% folder, but
+rem After  Windows XP, we redirect sleep commands to the internal *delay command:
+        goto :%OS
 
 
 :Windows_NT
 :10
 :11
 :7
-    if "%2" eq "silent" (goto :silent_1)
-rem %color_debug%  %+ echo %@CHAR[9201]%@CHAR[65039]%FAINT_ON% delay %* %FAINT_OFF%
-    %color_debug%  %+ echo %EMOJI_STOPWATCH%%ZZZZZZ%%FAINT_ON% delay %* %FAINT_OFF%
-    :silent_1
-    %color_normal% %+                                  delay %*
+    
+    rem Cute screen output, except when we don't want it:
+            if "%2" eq "silent" (goto :silent_1)
+                    rem  %ANSI_COLOR_DEBUG%%@CHAR[9201]%@CHAR[65039]%FAINT_ON% delay %* %FAINT_OFF%%ANSI_RESET%
+                    echo %ANSI_COLOR_DEBUG%%EMOJI_STOPWATCH%%ZZZZZZ%%FAINT_ON% delay %* %FAINT_OFF%%ANSI_RESET%
+            :silent_1
+
+    rem Do the actual sleep using the TCC internal DELAY command:
+             *delay %*
 goto :END
 
 
