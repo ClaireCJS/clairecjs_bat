d076cb2157fb524ef580f7e66b20561ad1f2e58d
ClioCJS <ClioCJS@gmail.com>
2024-10-07 06:13:37 -0400

    Mon 6:13AM: GRLwood - Keep It Average

---
+++ BAT-and-UTIL-files/app-volume-and-device-preferences-install-button-to-taskbar.bat
@@ -0,0 +1,19 @@
+@Echo OFF
+
+cls
+echo Trying to install button to go directly to app-volume-preferences ... to the QuickLaunchBar
+
+echo We will also install a copy of the icon to your desktop, so that it can be dragged to the launchbar in case the first step didn't work.
+
+echo Here we go!
+
+pause
+
+@copy c:\bat\app-volume-and-device-preferences.lnk "%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\app vol2.lnk"
+
+@copy c:\bat\app-volume-and-device-preferences.lnk %USERPROFILE%\OneDrive\Desktop
+
+echo All done! Good luck!
+pause
+
+
