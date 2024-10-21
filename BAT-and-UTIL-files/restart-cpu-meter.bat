6a0098fe9372c51f386cf0e422511d51fcedf5be
ClioCJS <ClioCJS@gmail.com>
2024-10-20 22:07:34 -0400

    Sun 10:07PM: The Dreadnoughts - Shiloh

--- BAT-and-UTIL-files/restart-cpu-meter.bat
+++ BAT-and-UTIL-files/restart-cpu-meter.bat
@@ -1,10 +1,10 @@
 @Echo OFF
 
-call removal ""Killing tray CPU-meter"
+call removal "Killing tray CPU-meter"
         call killIfRunning taskmgr taskmgr 
 
-call warning "Waiting 3 seconds..."
-        call wait 3
+rem call warning "Waiting 3 seconds..."
+        call wait 3 "(waiting 3 seconds)"
 
 call less_important "Re-starting tray CPU-meter..."
         start /min taskmgr
--- BAT-and-UTIL-files/restart-explorer.bat
+++ BAT-and-UTIL-files/restart-explorer.bat
@@ -1,15 +1,20 @@
 @Echo off
 
+rem Validate environment:
+        call validate-in-path removal kill wait start restart-cpu-meter
+        call validate-environment-variables OS 
+
 rem In XP, the tray-cpu meter that comes with taskmgr goes away unless you also restart taskman, so this will save us having to close it and reopen it:
-        if "%OS"=="XP" (call less_important "Killing task manager..." %+ kill /f taskmgr )
-        if "%OS"!="XP" (call less_important "Killing procexp..."      %+ kill /f procexp )
+        if "%OS"=="XP" (call removal "Killing task manager..." %+ kill /f taskmgr )
+        if "%OS"!="XP" (call removal "Killing procexp..."      %+ kill /f procexp )
 
 
-call less_important "Killing explorer..."
+rem call less_important "Killing explorer..."
+call     removal        "Killing explorer..."
         kill /f explorer
 
-call less_important "Wait 3 seconds..."
-        call sleep 3
+rem call less_important "Wait 3 seconds..."
+        call wait 3 "(waiting 3 seconds)"
 
 call less_important "Starting explorer..."
         start explorer
