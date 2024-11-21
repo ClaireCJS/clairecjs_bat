b66895fd2a1af7e8fef1eaa6fc80714c3383e45d
clairecjs <ClioCJS@gmail.com>
2024-11-21 00:21:50 -0500

    Thu 12:21AM: Equestria Nights - Princess Luna

--- BAT-and-UTIL-files-1/cursor-common.bat
+++ BAT-and-UTIL-files-1/cursor-common.bat
@@ -3,7 +3,7 @@
 
 iff     "%username%" eq "claire"  then
         call cursor-Claire.bat
-else if "%username%" eq "carolyn" then
+elseiff "%username%" eq "carolyn" then
         call cursor-Carolyn.bat
 else
         rem Don't do anything...   
