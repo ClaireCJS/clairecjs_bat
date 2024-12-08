1a457cf66a1d83cc3bd22cae86ab6d498d68f462
clairecjs <ClioCJS@gmail.com>
2024-12-08 13:47:54 -0500

    Sun 1:47PM: Sepultura - Show Me The Wrath

--- BAT-and-UTIL-files-1/get-lyrics-with-lyricsgenius-json-processor.pl
+++ BAT-and-UTIL-files-1/get-lyrics-with-lyricsgenius-json-processor.pl
@@ -1,31 +1,31 @@
 #!/usr/bin/perl
 
-use strict;													# use code strictness
-use warnings;												# use good warnings
-use JSON;													# import library functionality for JSON 
-use Encode;													# import library functionality for UTF-8 
-binmode STDOUT, ":encoding(UTF-8)";							# Set STDOUT to handle UTF-8
+use strict;                                                 # use code strictness
+use warnings;                                               # use good warnings
+use JSON;                                                   # import library functionality for JSON 
+use Encode;                                                 # import library functionality for UTF-8 
+binmode STDOUT, ":encoding(UTF-8)";                         # Set STDOUT to handle UTF-8
 
-my $json_text;												# downloaded .JSON file as string
-my $data;													# just the section of the JSON called “data” 
-my $lyrics;													# just the section of the data called “lyrics” 
+my $json_text;                                              # downloaded .JSON file as string
+my $data;                                                   # just the section of the JSON called “data” 
+my $lyrics;                                                 # just the section of the data called “lyrics” 
 
-# Get our data:												# Retrieve our input data from a JSON file
-$json_text = do { local $/; <STDIN> };					    # Read JSON input from STDIN
-$json_text = Encode::decode('UTF-8', $json_text);			# Decode the JSON text from UTF-8
-$data = decode_json($json_text);							# Parse JSON data
+# Get our data:                                             # Retrieve our input data from a JSON file
+$json_text = do { local $/; <STDIN> };                      # Read JSON input from STDIN
+$json_text = Encode::decode('UTF-8', $json_text);           # Decode the JSON text from UTF-8
+$data = decode_json($json_text);                            # Parse JSON data
 
-# Extract values:											# Extract lyrics and title
-my $lyrics = $data->{"lyrics"};								# Extract lyrics and title
-my $title  = $data->{"title" };								# Extract lyrics and title
+# Extract values:                                           # Extract lyrics and title
+my $lyrics = $data->{"lyrics"};                             # Extract lyrics and title
+my $title  = $data->{"title" };                             # Extract lyrics and title
 
-# Apply transformations:									# Apply transformations:	
-$lyrics =~ s/\\n/\n/ig;										# Replace “\n” with actual newlines
-$lyrics =~ s/[0-9] contributors?//ig;						# Remove  “X contributors” at the beginning
-$lyrics =~ s/\Q$title\E\s+lyrics//ig;						# Remove  “{title} lyrics” from the beginning
+# Apply transformations:                                    # Apply transformations:    
+$lyrics =~ s/\\n/\n/ig;                                     # Replace “\n” with actual newlines
+$lyrics =~ s/[0-9] contributors?//ig;                       # Remove  “X contributors” at the beginning
+$lyrics =~ s/\Q$title\E\s+lyrics//ig;                       # Remove  “{title} lyrics” from the beginning
 while ($lyrics =~  /(Embed|You might also like)\s*$/i) {    # Remove  “Embed” and “You might also like” until neither are present
-	   $lyrics =~ s/(Embed|You might also like)\s*$//i		# Remove  “Embed” and “You might also like” until neither are present
-}															# Remove  “Embed” and “You might also like” until neither are present
+       $lyrics =~ s/(Embed|You might also like)\s*$//i      # Remove  “Embed” and “You might also like” until neither are present
+}                                                           # Remove  “Embed” and “You might also like” until neither are present
 
-print $lyrics;												# Print modified lyrics
+print $lyrics;                                              # Print modified lyrics
 
