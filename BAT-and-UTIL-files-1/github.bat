b1e83b282aa1fe1f201ffd23159c32137f9e52f1
clairecjs <ClioCJS@gmail.com>
2024-12-15 14:08:56 -0500

    Sun 2:08PM: The Dreadnoughts - Za Śmierć Przyjaciela

--- BAT-and-UTIL-files-1/github.bat
+++ BAT-and-UTIL-files-1/github.bat
@@ -4,7 +4,9 @@
 echo.
 call important "Going to Claire's github"
 
-iff exist "c:\bat\%@UNQUOTE[%1]" then
+iff "%1" eq "audio-processing-batch-NOTES.txt" then
+        echo call see-latest-version-of-BAT-on-github.bat "`notes%20-%20audio%20processing.txt`"
+elseiff exist "c:\bat\%@UNQUOTE[%1]" .or. exist "c:\notes\%@UNQUOTE[%1]" then
         call see-latest-version-of-BAT-on-github.bat %*
 else
         https://github.com/ClaireCJS/clairecjs_bat/
--- BAT-and-UTIL-files-2/see-latest-version-of-BAT-on-github.bat
+++ BAT-and-UTIL-files-2/see-latest-version-of-BAT-on-github.bat
@@ -1,8 +1,8 @@
 @Echo OFF
 @on break cancel
 
-iff exist "c:\bat\%@UNQUOTE[%1]" then
-        https://github.com/ClaireCJS/clairecjs_bat/blob/main/BAT-and-UTIL-files-1/%@UNQUOTE[%1]
+iff exist "c:\bat\%@UNQUOTE[%1]" .or. exist "c:\notes\%@UNQUOTE[%1]" then
+        echo https://github.com/ClaireCJS/clairecjs_bat/blob/main/BAT-and-UTIL-files-1/%@UNQUOTE[%1]
 else 
         call warning "%@UNQUOTE[%1].bat doesn't exist!"
         https://github.com/ClaireCJS/clairecjs_bat/
