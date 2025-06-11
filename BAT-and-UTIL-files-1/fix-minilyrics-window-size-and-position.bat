2eea8c46abaf92139085932da54c3fbebafd5c23
clairecjs <ClioCJS@gmail.com>
2025-06-11 09:49:53 -0400

    Wed 9:49AM: None

--- BAT-and-UTIL-files-1/fix-minilyrics-window-size-and-position.bat
+++ BAT-and-UTIL-files-1/fix-minilyrics-window-size-and-position.bat
@@ -5,8 +5,11 @@
 rem activate "minilyrics" pos=-1059,-1085,700,670 %+ rem this works but if it results in a not-high-enough, halfway-off-the-screen-to-the-left, maybe do this one instead:
 
 rem 20241217: before getting new tv: 1080p+1080p main+4K upper-right+1080p monitor
-    activate "minilyrics"        pos=-0708,-1085,800,800
-    activate "Floating Lyrics"   pos=1208,-2157,1275,200
+        rem  ate "minilyrics"      pos=-0708,-1085,800,800
+        activate "Floating Lyrics" pos=1208,-2157,1275,200
+
+rem 20250614: Same setup, but something has changed....
+        activate "minilyrics"      pos=-0820,-1080,1050,1050
 
 rem used to do this here: call fix-winamp %*
 
